b47659fcdf69a0d49742cd17840f897dd6872e21
Moaz Amr <moazamralsaid@gmail.com>
2025-03-10 15:13:28 +0200

    add test_app.py

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import pytest
+from app import app
+
+def test_request():
+    TestResponse = app.test_client().get('/')
+    response_status_code = TestResponse.status_code
+    response_data = TestResponse.data
+    assert response_status_code == 200, 'Response invalid !'
+    assert response_data == b'<h1 style = "color:green;">Hello world !</h1>'
